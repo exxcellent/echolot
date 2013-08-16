exxcellent/echolot

commit 665e98c90a2925ca2643f8f4802017d5e4c450f4

Travis CI support, JSLint tests, JS code fixes & formatting

echolot-webcontainer/src/main/resources/js/Sync.BarChart.js
@@ -9,7 +9,7 @@
  * @version 1.0
  */
 exxcellent.BarChart = Core.extend(Echo.Component, {
-    $load: function() {
+    $load: function () {
         Echo.ComponentFactory.registerType('exxcellent.BarChart', this);
     },
     $static: {
@@ -42,12 +42,12 @@ exxcellent.BarChart = Core.extend(Echo.Component, {
         BAR_SELECTION: 'barSelection'
     },
     componentType: 'exxcellent.BarChart',
-    doSelectBar : function(barIdentifier) {
-        // 	Notify table row select listeners.
+    doSelectBar: function (barIdentifier) {
+        // Notify table row select listeners.
         this.fireEvent({
-            type : exxcellent.BarChart.BAR_SELECTION,
-            source : this,
-            data : barIdentifier
+            type: exxcellent.BarChart.BAR_SELECTION,
+            source: this,
+            data: barIdentifier
         });
     }
 
@@ -67,12 +67,12 @@ exxcellent.model.BarChartModel = Core.extend({
     // e.g. [[bar1, bar2],[bar3, bar4]]
     barValues: null,
 
-    $construct : function(barValues) {
+    $construct: function (barValues) {
         this.barValues = barValues;
     },
 
     /** Return the string representation of this PieModel. */
-    toString : function() {
+    toString: function () {
         return 'I am a BAR-Chart';
     }
 });
@@ -86,7 +86,7 @@ exxcellent.model.Bar = Core.extend({
     label: '',
     color: null,
 
-    $construct : function(value, label, identifier, color) {
+    $construct: function (value, label, identifier, color) {
         this.value = value;
         this.label = label;
         this.identifier = identifier;
@@ -94,7 +94,7 @@ exxcellent.model.Bar = Core.extend({
     },
 
     /** Return the string representation of this Bar */
-    toString : function() {
+    toString: function () {
         return 'Label:' + this.label + ' value:' + this.value + ' color:' + this.color + ' identifier:' + this.identifier;
     }
 });
@@ -147,7 +147,7 @@ exxcellent.model.BarChartLayout = Core.extend({
  * @version 1.0
  */
 exxcellent.BarChartSync = Core.extend(Echo.Render.ComponentSync, {
-    $load: function() {
+    $load: function () {
         Echo.Render.registerPeer("exxcellent.BarChart", this);
     },
 
@@ -162,7 +162,7 @@ exxcellent.BarChartSync = Core.extend(Echo.Render.ComponentSync, {
      * @param update
      * @param parentElement
      */
-    renderAdd: function(update, parentElement) {
+    renderAdd: function (update, parentElement) {
         // we only need to create the holder' div for raphael - the rest will be done in renderDisplay
         this._div = document.createElement("div");
 
@@ -177,7 +177,7 @@ exxcellent.BarChartSync = Core.extend(Echo.Render.ComponentSync, {
     /**
      * Render the bar itself to the containerDiv
      */
-    renderDisplay: function() {
+    renderDisplay: function () {
         var self = this; // it's always good to know who you are in JS :-)
         if (this._raphael) {
             // if we have already a raphael, we do nothing att all - it's just a simple refresh
@@ -203,49 +203,45 @@ exxcellent.BarChartSync = Core.extend(Echo.Render.ComponentSync, {
         // Maybe we could use some JS-Voodoo to have just on call with +-h :-) - but it seems to work
         if (barChartLayout.barAlignment == 'horizontal') {
             this._raphael.g.hbarchart(
-                    barChartLayout.xgap, // x-gap
-                    barChartLayout.ygap, // y-gap
-                    barChartLayout.width, // width
-                    barChartLayout.height, // height
-                    null, // original 'values' : Changed the behavior tu use this in Echo3 - the model is transported to simile via the attribute 'barChartModel'
+                barChartLayout.xgap, // x-gap
+                barChartLayout.ygap, // y-gap
+                barChartLayout.width, // width
+                barChartLayout.height, // height
+                null, // original 'values' : Changed the behavior tu use this in Echo3 - the model is transported to simile via the attribute 'barChartModel'
                 /*options*/
                 /**/{
-                /*->*/stacked: barChartLayout.stacked, // isStacked
-                /*->*/type: barChartLayout.headType, // headType
-                /*->*/vgutter: 0, // the empty-spacing vertically
-                /*->*/showTooltip: barChartLayout.showTooltip
-                /**/},
-                    barChartModel // the barModel (array of array of exxcellent.model.Bar
-                    ).// callbacks
-                    hover(// MouseHovering:
+                    /*->*/stacked: barChartLayout.stacked, // isStacked
+                    /*->*/type: barChartLayout.headType, // headType
+                    /*->*/vgutter: 0, // the empty-spacing vertically
+                    /*->*/showTooltip: barChartLayout.showTooltip
+                    /**/},
+                barChartModel // the barModel (array of array of exxcellent.model.Bar
+            ).hover(// MouseHovering:
                     this._fin(barChartLayout, raphael_self), // MouseIn
                     this._fout(barChartLayout, raphael_self) // MouseOut
-                    ).// end of MouseHovering
-                    click(this._click(this, raphael_self))// Click-Handler
-                    ; // -> that's it :-)
+                ).click(this._click(this, raphael_self))// Click-Handler
+            ; // -> that's it :-)
         } else if (barChartLayout.barAlignment == 'vertical') {
             // just some little difference, we call barchart instead of hbarchart
             this._raphael.g.barchart(
-                    barChartLayout.xgap, // x-gap
-                    barChartLayout.ygap, // y-gap
-                    barChartLayout.width, // width
-                    barChartLayout.height, // height
-                    null, // original 'values' : Changed the behavior tu use this in Echo3 - the model is transported to simile via the attribute 'barChartModel'
+                barChartLayout.xgap, // x-gap
+                barChartLayout.ygap, // y-gap
+                barChartLayout.width, // width
+                barChartLayout.height, // height
+                null, // original 'values' : Changed the behavior tu use this in Echo3 - the model is transported to simile via the attribute 'barChartModel'
                 /*options*/
                 /**/{
-                /*->*/stacked: barChartLayout.stacked, // isStacked
-                /*->*/type: barChartLayout.headType, // headType
-                /*->*/vgutter: 0, // the empty-spacing vertically
-                /*->*/showTooltip: barChartLayout.showTooltip
-                /**/},
-                    barChartModel // the barModel (array of array of exxcellent.model.Bar
-                    ).// callbacks
-                    hover(// MouseHovering:
+                    /*->*/stacked: barChartLayout.stacked, // isStacked
+                    /*->*/type: barChartLayout.headType, // headType
+                    /*->*/vgutter: 0, // the empty-spacing vertically
+                    /*->*/showTooltip: barChartLayout.showTooltip
+                    /**/},
+                barChartModel // the barModel (array of array of exxcellent.model.Bar
+            ).hover(// MouseHovering:
                     this._fin(barChartLayout, raphael_self), // MouseIn
                     this._fout(barChartLayout, raphael_self) // MouseOut
-                    ).// end of MouseHovering
-                    click(this._click(this, raphael_self))// Click-Handler
-                    ; // -> that's it :-)
+                ).click(this._click(this, raphael_self))// Click-Handler
+            ; // -> that's it :-)
 
         }
     },
@@ -255,7 +251,7 @@ exxcellent.BarChartSync = Core.extend(Echo.Render.ComponentSync, {
      * We clean all allocated data
      * @param update
      */
-    renderDispose: function(update) {
+    renderDispose: function (update) {
         // setting all globals to null - we don't want them to live forever ;-)
         this._div = null;
         this._raphael = null;
@@ -265,7 +261,7 @@ exxcellent.BarChartSync = Core.extend(Echo.Render.ComponentSync, {
      *
      * @param update
      */
-    renderUpdate: function(update) {
+    renderUpdate: function (update) {
         // Brut-force - just create everything new
         // we could think about of some ajax-feature, but there is no need so far
         var element = this._div;
@@ -284,14 +280,14 @@ exxcellent.BarChartSync = Core.extend(Echo.Render.ComponentSync, {
      * Callback for MouseIn
      * @param raphael
      */
-    _fin : function (barChartLayout, raphael) {
-        return function() {
+    _fin: function (barChartLayout, raphael) {
+        return function () {
             if (barChartLayout.showPopup) {
                 var direction = 'top';
                 if (barChartLayout.autoAdjustPopup) {
                     // damn IE vs. FF... IE defines canvas.clientWidth and FF paper.width... :-(
-                    var widthToTest = (this.paper.canvas.clientWidth && this.paper.canvas.clientWidth != 0) ? this.paper.canvas.clientWidth : this.paper.width;
-                    var heightToTest = (this.paper.canvas.clientHeight && this.paper.canvas.clientHeight != 0) ? this.paper.canvas.clientHeight : this.paper.height;
+                    var widthToTest = (this.paper.canvas.clientWidth && this.paper.canvas.clientWidth !== 0) ? this.paper.canvas.clientWidth : this.paper.width;
+                    var heightToTest = (this.paper.canvas.clientHeight && this.paper.canvas.clientHeight !== 0) ? this.paper.canvas.clientHeight : this.paper.height;
                     // we try to adjust the PopUp
                     if (this.bar.x > widthToTest - 50 && this.bar.y > heightToTest - 50) {
                         direction = 'top-right';
@@ -308,9 +304,9 @@ exxcellent.BarChartSync = Core.extend(Echo.Render.ComponentSync, {
                 var popupTextFill = {fill: barChartLayout.popupForeground};
                 var popUpFont = barChartLayout.popupFont;
                 this.popUpLabel = raphael.text(0, 0, this.bar.Bar.label || "0").attr(popupTextFill).attr(popUpFont);
-                this.popUpBubble = raphael.popup(this.bar.x, this.bar.y, this.popUpLabel, direction).attr({fill: barChartLayout.popupBackground, stroke: barChartLayout.popupBorderColor, "stroke-width": 2, "fill-opacity": .7});
+                this.popUpBubble = raphael.popup(this.bar.x, this.bar.y, this.popUpLabel, direction).attr({fill: barChartLayout.popupBackground, stroke: barChartLayout.popupBorderColor, "stroke-width": 2, "fill-opacity": 0.7});
             }
-        }
+        };
 
     },
 
@@ -318,9 +314,9 @@ exxcellent.BarChartSync = Core.extend(Echo.Render.ComponentSync, {
      * Callback for MouseOut
      * @param raphael
      */
-    _fout : function (barChartLayout, raphael) {
+    _fout: function (barChartLayout, raphael) {
         // we certainly don't need the variable raphael... bu who knows what's later on
-        return function() {
+        return function () {
             if (barChartLayout.showPopup) {
                 this.popUpLabel.animate({opacity: 0}, 300, function () {
                     this.remove();
@@ -329,7 +325,7 @@ exxcellent.BarChartSync = Core.extend(Echo.Render.ComponentSync, {
                     this.remove();
                 });
             }
-        }
+        };
     },
 
     /**
@@ -337,12 +333,12 @@ exxcellent.BarChartSync = Core.extend(Echo.Render.ComponentSync, {
      * @param self
      * @param raphael
      */
-    _click : function (self, raphael) {
+    _click: function (self, raphael) {
         // we certainly don't need the variable raphael... bu who knows what's later on
-        return function() {
+        return function () {
             // throw the action 'doSelectBar' with the identifier
             self.component.doSelectBar(this.bar.Bar.identifier);
-        }
+        };
 
     },
 
@@ -350,7 +346,7 @@ exxcellent.BarChartSync = Core.extend(Echo.Render.ComponentSync, {
      * Returns the BarChartLayout - this Objects helps to deal with Layout
      * @see exxcellent.model.BarChartLayout for further details
      */
-    _getBarChartLayout : function() {
+    _getBarChartLayout: function () {
         // We try to get all values defined by the user. For Fallback there are defaults defined where suitable
         var barChartLayout = new exxcellent.model.BarChartLayout();
         barChartLayout.width = this.component.render(exxcellent.BarChart.WIDTH) || 100;
@@ -374,7 +370,7 @@ exxcellent.BarChartSync = Core.extend(Echo.Render.ComponentSync, {
      * Get the BarChart-Model.
      * In case of a JSON-Object we parse it to create the exxcellent.model.BarChartModel
      */
-    _getBarChartModel : function () {
+    _getBarChartModel: function () {
         var value = this.component.render(exxcellent.BarChart.BAR_CHART_MODEL);
         if (value instanceof exxcellent.model.BarChartModel) {
             return value;
@@ -389,7 +385,7 @@ exxcellent.BarChartSync = Core.extend(Echo.Render.ComponentSync, {
      * @param {} json the string to be transformed into an object
      * @return {} the object
      */
-    _fromJsonString : function(jsonStr) {
+    _fromJsonString: function (jsonStr) {
         return JSON.parse(jsonStr);
     },
 
@@ -399,7 +395,7 @@ exxcellent.BarChartSync = Core.extend(Echo.Render.ComponentSync, {
      * @param {} object the object to be transformed into string
      * @return {} the json string
      */
-    _toJsonString : function(object) {
+    _toJsonString: function (object) {
         return JSON.stringify(object);
     },
 
@@ -408,20 +404,22 @@ exxcellent.BarChartSync = Core.extend(Echo.Render.ComponentSync, {
      * @param font {Echo.Sync.Font} the font to render as notifier compatible font
      * @return the raphael compatible font
      */
-    _renderFont: function(font) {
-        if (font == null) return null;
-        var fontByEcho = {
-            style: new Object()
+    _renderFont: function (font) {
+        if (font === null) {
+            return null;
         }
+        var fontByEcho = {
+            style: {}
+        };
         Echo.Sync.Font.render(font, fontByEcho);
         var echoStyle = fontByEcho.style;
 
         return {
-            'font' : echoStyle.fontSize + ' ' + echoStyle.fontFamily,
-            'font-family' : echoStyle.fontFamily,
-            'font-size' : echoStyle.fontSize,
-            'font-style' : echoStyle.fontStyle
-        }
+            'font': echoStyle.fontSize + ' ' + echoStyle.fontFamily,
+            'font-family': echoStyle.fontFamily,
+            'font-size': echoStyle.fontSize,
+            'font-style': echoStyle.fontStyle
+        };
 
     }
 });

echolot-webcontainer/src/main/resources/js/Sync.LineChart.js
@@ -10,7 +10,7 @@
  */
 
 exxcellent.LineChart = Core.extend(Echo.Component, {
-    $load: function() {
+    $load: function () {
         Echo.ComponentFactory.registerType('exxcellent.LineChart', this);
     },
     $static: {
@@ -19,7 +19,6 @@ exxcellent.LineChart = Core.extend(Echo.Component, {
 
         XAXIS_SECTORS: 'xaxisSectors',
         YAXIS_SECTORS: 'yaxisSectors',
-
 
 
         FILL_CHART: 'fillChart',
@@ -47,12 +46,12 @@ exxcellent.LineChart = Core.extend(Echo.Component, {
         POINT_SELECTION: 'pointSelection'
     },
     componentType: 'exxcellent.LineChart',
-    doSelectPoint : function(pointIdentifier) {
-        // 	Notify table row select listeners.
+    doSelectPoint: function (pointIdentifier) {
+        // Notify table row select listeners.
         this.fireEvent({
-            type : exxcellent.LineChart.POINT_SELECTION,
-            source : this,
-            data : pointIdentifier
+            type: exxcellent.LineChart.POINT_SELECTION,
+            source: this,
+            data: pointIdentifier
         });
     }
 });
@@ -69,12 +68,12 @@ exxcellent.model.LineChartModel = Core.extend({
     /** Should this PIE be animated */
     points: null,
 
-    $construct : function(points) {
+    $construct: function (points) {
         this.points = points;
     },
 
     /** Return the string representation of this PieModel. */
-    toString : function() {
+    toString: function () {
         return 'I am a LineChartModel';
     }
 });
@@ -87,12 +86,12 @@ exxcellent.model.AxisModel = Core.extend({
     yAxisValues: null,
 
 
-    $construct : function(xAxisValues, yAxisValues) {
+    $construct: function (xAxisValues, yAxisValues) {
         this.xAxisValues = xAxisValues;
         this.yAxisValues = yAxisValues;
     },
 
-    toString : function() {
+    toString: function () {
         return 'AxisModel - xAxis: ' + this.xAxisValues + ' yAxis: ' + this.yAxisValues;
     }
 });
@@ -107,7 +106,7 @@ exxcellent.model.Point = Core.extend({
 
     label: '',
 
-    $construct : function(xValue, yValue, label, identifier) {
+    $construct: function (xValue, yValue, label, identifier) {
         this.xValue = xValue;
         this.yValue = yValue;
         this.label = label;
@@ -115,7 +114,7 @@ exxcellent.model.Point = Core.extend({
     },
 
     /** Return the string representation of this Point */
-    toString : function() {
+    toString: function () {
         return 'Label:' + this.label + ' xValue:' + this.xValue + ' yValue:' + this.yValue;
     }
 });
@@ -144,87 +143,87 @@ exxcellent.model.LineChartLayout = Core.extend({
     width: 300,
     height: 200,
 
-    $construct : function() {
+    $construct: function () {
         // nothing to do yet
     },
 
-    isDrawGrid : function() {
+    isDrawGrid: function () {
         return this.drawGrid;
     },
 
-    getGridColor : function() {
+    getGridColor: function () {
         return this.gridColor;
     },
 
-    getXaxisSectors : function() {
+    getXaxisSectors: function () {
         return this.xaxisSectors;
     },
 
-    getYaxisSectors : function() {
+    getYaxisSectors: function () {
         return this.yaxisSectors;
     },
 
-    getAxisFont : function() {
+    getAxisFont: function () {
         return this.axisFont;
     },
 
-    getForeground : function() {
+    getForeground: function () {
         return this.foreground;
     },
 
-    isShowPopup : function() {
+    isShowPopup: function () {
         return this.showPopup;
     },
 
-    getPopupBackground : function() {
+    getPopupBackground: function () {
         return this.popupBackground;
     },
 
-    getPopupBorderColor : function() {
+    getPopupBorderColor: function () {
         return this.popupBorderColor;
     },
 
-    getPopupForeground : function() {
+    getPopupForeground: function () {
         return this.popupForeground;
     },
 
-    getPopupFont : function() {
+    getPopupFont: function () {
         return this.popupFont;
     },
 
-    isFillChart : function() {
+    isFillChart: function () {
         return this.fillChart;
     },
 
-    getLineColor : function() {
+    getLineColor: function () {
         return this.lineColor;
     },
 
-    getDotColor : function() {
+    getDotColor: function () {
         return this.dotColor;
     },
 
-    getInterpolation : function() {
+    getInterpolation: function () {
         return this.interpolation;
     },
 
-    getXscaleMax : function() {
+    getXscaleMax: function () {
         return this.xscaleMax;
     },
 
-    getYscaleMax: function() {
+    getYscaleMax: function () {
         return this.yscaleMax;
     },
 
-    getWidth : function() {
+    getWidth: function () {
         return this.width;
     },
 
-    getHeight : function() {
+    getHeight: function () {
         return this.height;
     },
 
-    toString : function() {
+    toString: function () {
         return 'LineChartModel';
     }
 });
@@ -240,7 +239,7 @@ exxcellent.model.LineChartLayout = Core.extend({
  * @version 1.0
  */
 exxcellent.LineChartSync = Core.extend(Echo.Render.ComponentSync, {
-    $load: function() {
+    $load: function () {
         Echo.Render.registerPeer('exxcellent.LineChart', this);
     },
 
@@ -253,7 +252,7 @@ exxcellent.LineChartSync = Core.extend(Echo.Render.ComponentSync, {
     _raphael: null,
 
     /** @see Echo.Render.ComponentSync#renderAdd */
-    renderAdd: function(update, parentElement) {
+    renderAdd: function (update, parentElement) {
         this._div = document.createElement("div");
         var background = this.component.render(exxcellent.LineChart.BACKGROUND);
         if (background) {
@@ -265,7 +264,7 @@ exxcellent.LineChartSync = Core.extend(Echo.Render.ComponentSync, {
     },
 
     /** @see Echo.Render.ComponentSync#renderDisplay */
-    renderDisplay: function() {
+    renderDisplay: function () {
         var self = this;
         if (this._raphael) {
             // if we have already a raphael, we do nothing att all - it's just a simple refresh
@@ -298,7 +297,7 @@ exxcellent.LineChartSync = Core.extend(Echo.Render.ComponentSync, {
      * We clean all allocated data
      * @param update
      */
-    renderDispose: function(update) {
+    renderDispose: function (update) {
         this._div = null;
         this._raphael = null;
     },
@@ -308,7 +307,7 @@ exxcellent.LineChartSync = Core.extend(Echo.Render.ComponentSync, {
      *
      * @param update
      */
-    renderUpdate: function(update) {
+    renderUpdate: function (update) {
         // Brut-force - just create everything new
         var element = this._div;
         this._raphael = null; // we set the Raphael to null to force a redraw
@@ -322,7 +321,7 @@ exxcellent.LineChartSync = Core.extend(Echo.Render.ComponentSync, {
     /**
      * Returns the LineChartLayout - this Objects helps to deal with Layout
      */
-    _getLineChartLayout : function() {
+    _getLineChartLayout: function () {
         var lineChartLayout = new exxcellent.model.LineChartLayout();
         lineChartLayout.showPopup = this.component.render(exxcellent.LineChart.SHOW_POPUP);
         lineChartLayout.popupBackground = this.component.render(exxcellent.LineChart.POPUP_BACKGROUND);
@@ -350,7 +349,7 @@ exxcellent.LineChartSync = Core.extend(Echo.Render.ComponentSync, {
      * Get the LineChart-Model.
      * In case of a JSON-Object we parse it to create the exxcellent.model.PieModel
      */
-    _getLineChartModel : function () {
+    _getLineChartModel: function () {
         var value = this.component.render(exxcellent.LineChart.LINE_CHART_MODEL);
         if (value instanceof exxcellent.model.LineChartModel) {
             return value;
@@ -363,7 +362,7 @@ exxcellent.LineChartSync = Core.extend(Echo.Render.ComponentSync, {
      * Get the Axis-Model.
      * In case of a JSON-Object we parse it to create the exxcellent.model.PieModel
      */
-    _getAxisModel : function () {
+    _getAxisModel: function () {
         var value = this.component.render(exxcellent.LineChart.AXIS_MODEL);
         if (value instanceof exxcellent.model.AxisModel) {
             return value;
@@ -378,7 +377,7 @@ exxcellent.LineChartSync = Core.extend(Echo.Render.ComponentSync, {
      * @param {} json the string to be transformed into an object
      * @return {} the object
      */
-    _fromJsonString : function(jsonStr) {
+    _fromJsonString: function (jsonStr) {
         return JSON.parse(jsonStr);
     },
 
@@ -388,7 +387,7 @@ exxcellent.LineChartSync = Core.extend(Echo.Render.ComponentSync, {
      * @param {} object the object to be transformed into string
      * @return {} the json string
      */
-    _toJsonString : function(object) {
+    _toJsonString: function (object) {
         return JSON.stringify(object);
     },
 
@@ -396,10 +395,10 @@ exxcellent.LineChartSync = Core.extend(Echo.Render.ComponentSync, {
      * Callback - a click occured
      * @param self
      */
-    _clickCallback : function(self) {
-        return function() {
+    _clickCallback: function (self) {
+        return function () {
             self.component.doSelectPoint(this.identifier);
-        }
+        };
     },
 
     /**
@@ -407,20 +406,22 @@ exxcellent.LineChartSync = Core.extend(Echo.Render.ComponentSync, {
      * @param font {Echo.Sync.Font} the font to render as notifier compatible font
      * @return the raphael compatible font
      */
-    _renderFont: function(font) {
-        if (font == null) return null;
-        var fontByEcho = {
-            style: new Object()
+    _renderFont: function (font) {
+        if (font === null) {
+            return null;
         }
+        var fontByEcho = {
+            style: {}
+        };
         Echo.Sync.Font.render(font, fontByEcho);
         var echoStyle = fontByEcho.style;
 
         return {
-            'font' : echoStyle.fontSize + ' ' + echoStyle.fontFamily,
-            'font-family' : echoStyle.fontFamily,
-            'font-size' : echoStyle.fontSize,
-            'font-style' : echoStyle.fontStyle
-        }
+            'font': echoStyle.fontSize + ' ' + echoStyle.fontFamily,
+            'font-family': echoStyle.fontFamily,
+            'font-size': echoStyle.fontSize,
+            'font-style': echoStyle.fontStyle
+        };
 
     }
 });

commit f808b2b93d2d5bb1db0e3d937e42ab6fd32819e2

Bugfix in font handling of Pie, Bar and Line Charts and SuggestField
Bug was introduced in commit 665e98c90a292, when the test for `font == null`
was changed to `font === null`. However, font was never `null` but actually
`undefined`, which the changed comparison did not detect anymore.

Also fixed wrong constructor use in JS PieChart test.

echolot-webcontainer/src/main/resources/js/Sync.BarChart.js
@@ -405,7 +405,7 @@ exxcellent.BarChartSync = Core.extend(Echo.Render.ComponentSync, {
      * @return the raphael compatible font
      */
     _renderFont: function (font) {
-        if (font === null) {
+        if (!font) {
             return null;
         }
         var fontByEcho = {

echolot-webcontainer/src/main/resources/js/Sync.LineChart.js
@@ -407,7 +407,7 @@ exxcellent.LineChartSync = Core.extend(Echo.Render.ComponentSync, {
      * @return the raphael compatible font
      */
     _renderFont: function (font) {
-        if (font === null) {
+        if (!font) {
             return null;
         }
         var fontByEcho = {

echolot-webcontainer/src/main/resources/js/Sync.PieChart.js
@@ -485,7 +485,7 @@ exxcellent.PieChartSync = Core.extend(Echo.Render.ComponentSync, {
      * @return the raphael compatible font
      */
     _renderFont: function (font) {
-        if (font === null) {
+        if (!font) {
             return null;
         }
         var fontByEcho = {

echolot-webcontainer/src/main/resources/js/Sync.SuggestField.js
@@ -343,7 +343,7 @@ exxcellent.SuggestFieldSync = Core.extend(Echo.Sync.TextComponent, {
      * @param color
      */
     _getFontAsStyle: function (font, color) {
-        if (font === null) {
+        if (!font) {
             return '';
         }
         var style = 'style = "' +

echolot-webcontainer/src/test/web/testscreen/PieChartTest.js
@@ -139,17 +139,17 @@ exxcellent.test.PieChartTest = Core.extend({
     _createPieModel_1: function () {
         // the sectors
         var sectors = [
-            new exxcellent.model.PieSector("Blubber", 260, "Blubber PopUp", true, '#33FF99', 'Abbrev 1', 10)
+            new exxcellent.model.PieSector("Blubber", 260, "Blubber PopUp", true, '#33FF99', 'Abbrev 1', "#000", 10)
             ,
-            new exxcellent.model.PieSector("Bar", 1000, "Bar PopUp", true, '#666633', 'Abbrev 3', null, 12)
+            new exxcellent.model.PieSector("Bar", 1000, "Bar PopUp", true, '#666633', 'Abbrev 3', null, "#000", 12)
             ,
-            new exxcellent.model.PieSector("One", 1, "one", true, '#666633', 'Abbrev 3', null, 12)
+            new exxcellent.model.PieSector("One", 1, "one", true, '#666633', 'Abbrev 3', null, "#000", 12)
             ,
-            new exxcellent.model.PieSector("Bar", 500, "Bar PopUp", true, '#666633', 'Abbrev 3', null, 12)
+            new exxcellent.model.PieSector("Bar", 500, "Bar PopUp", true, '#666633', 'Abbrev 3', null, "#000", 12)
             ,
-            new exxcellent.model.PieSector("Also one", 1, "also one", true, '#666633', 'Abbrev 3', null, 12)
+            new exxcellent.model.PieSector("Also one", 1, "also one", true, '#666633', 'Abbrev 3', null, "#000", 12)
             ,
-            new exxcellent.model.PieSector("zero2", 0, "zero", true, '#666633', 'Abbrev 3', null, 12)
+            new exxcellent.model.PieSector("zero2", 0, "zero", true, '#666633', 'Abbrev 3', null, "#000", 12)
         ];
 
         // return the whole thing